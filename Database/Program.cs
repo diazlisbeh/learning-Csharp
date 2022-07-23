59aad1ab1e08f2d2b59a9bed49c7de6197ca46a6
Lisbeth Diaz <lizbeths.robles@gmail.com>
2022-07-23 00:59:56 -0400

    commit

--- Database/Program.cs
+++ Database/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DB db = new DB(@"containers-us-west-66.railway.app","railway","root","svq9dWLslBbrLQ5iolIb");
+            db.Connect();
+            db.Close();
+        }
+    }
+}
